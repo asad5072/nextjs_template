8acca76e5ab407d45d577f47eb298c32efab818c
milton <miltonkazi2004@gmail.com>
2025-06-28 21:56:30 +0600

    updated

---
+++ frontend/hooks/useApi.ts
@@ -0,0 +1,49 @@
+"use client";
+import axios from "axios";
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL + "/api";
+
+export function useApi() {
+	// GET method
+	const get = async (endpoint: string) => {
+		const response = await axios.get(baseURL + endpoint);
+		if (response.status !== 200) {
+			throw new Error(`GET ${endpoint} failed: ${response.statusText}`);
+		}
+		return response.data;
+	};
+
+	// POST method
+	const post = async (endpoint: string, data: any) => {
+		const response = await axios.post(baseURL + endpoint, data);
+		if (response.status < 200 || response.status >= 300) {
+			throw new Error(`POST ${endpoint} failed: ${response.statusText}`);
+		}
+		return response.data;
+	};
+
+	// PUT method
+	const put = async (endpoint: string, data: any) => {
+		const response = await axios.put(baseURL + endpoint, data);
+		if (response.status < 200 || response.status >= 300) {
+			throw new Error(`PUT ${endpoint} failed: ${response.statusText}`);
+		}
+		return response.data;
+	};
+
+	// DELETE method
+	const del = async (endpoint: string) => {
+		const response = await axios.delete(baseURL + endpoint);
+		if (response.status < 200 || response.status >= 300) {
+			throw new Error(`DELETE ${endpoint} failed: ${response.statusText}`);
+		}
+		return response.data;
+	};
+
+	return {
+		get,
+		post,
+		put,
+		del,
+	};
+}
